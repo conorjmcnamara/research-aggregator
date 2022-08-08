d8035cf6cfe719f25cdfa546948dbf1374d72754
Conor McNamara <conormcnamara245@gmail.com>
2022-08-08 23:50:54 +0100

    Add abstracts and web URLs

---
+++ static/script.js
@@ -0,0 +1,24 @@
+function showExtraData(abstract, title) {
+    // check if the abstract is not displayed
+    if (document.getElementById(abstract).style.display == "none") {
+        // display the abstract
+        document.getElementById(abstract).style.display = "table-cell";
+
+        // add the URL hyperlink
+        setTimeout(function() {
+            document.getElementById(title).setAttribute("href", title);
+            document.getElementById(title).setAttribute("target", "_blank");
+            document.getElementById(title).style.color = "black";
+        }, 10);
+    }
+    
+    else {
+        // hide the abstract
+        document.getElementById(abstract).style.display = "none";
+
+        // remove the URL hyperlink
+        setTimeout(function() {
+            document.getElementById(title).removeAttribute("href");
+        }, 10);
+    }
+}
