d4b714a8a44433196621b0c09e2156c6a2465cea
Conor McNamara <conormcnamara245@gmail.com>
2023-02-03 15:35:27 +0000

    Add TypeScript React

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+import './App.css';
+import { Navigation } from './components/Navigation';
+
+const App: FC = () => {
+  return (
+    <div className="App">
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+export const Navigation: FC = () => {
+    return (
+        <div>
+            <h1>Test</h1>
+        </div>
+    );
+}
